bb72338bf9d0d4a4825b60a67c3650f64adae0ad
ZERLLEL <greatzerllel2@gmail.com>
2022-09-28 14:22:18 +0000

    reproductor v2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-/* 
-import 'bootstrap/dist/css/bootstrap.min.css'; // CSS
-import 'bootstrap/dist/js/bootstrap.bundle'; // JS
-import '@fortawesome/fontawesome-free/css/all.min.css' // FONTAWESOME
-import './App.css'; // MY CSS
-
-import * as data from './data/info'; // INFO, DATA OR ALL YOU NEED TO ADD IN DOM IN TEXT, IF U HAV LOCAL IMG, U NEED ADD IMG FOLDER IN >>"public FOLDER"<< AND PASTE THERE TO CALL THE IMAGES FROM THERE
-
-import { Main } from './Main'; // PRINCIPAL COMPONENT
-
-const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(<Main {...data} />); // Spread operator */
-
 //import react into the bundle
 import React from 'react';
 import ReactDOM from 'react-dom/client';
